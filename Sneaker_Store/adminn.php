e654513f6001c710366fb9a9f176b714ef306911
qucaiduhh <queshahzulqarnain@gmail.com>
2025-04-15 14:48:55 +0800

    Add files via upload

---
+++ Sneaker_Store/adminn.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang= "en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewsport" content ="width=device-width, intial-scale=1.0">
+    <tittle>Payment Management </tittle>
+    <link rel="stylesheet" href= "admin.css">
+</head>
+<body>
+    <h2>Payments </h2>
+<table>
+    <tr>
+        <th>No.</th>
+        <th>Name</th>
+        <th>Phone</th>
+        <th>Address</th>
+        <th>Card Number</th>
+        <th>Expiry Month</th>
+        <th>Expiry Year</th>
+        <th>CVV</th>
+        </tr>
+        <?php
+        $servername = "localhost";
+        $username= "root";
+        $password = "";
+        $dbname = "sneakerstoree";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if  ($conn->connect_error) {
+             die("Connection failed: " . $conn->connect_error);
+         }
+
+        $sql = "SELECT name, phone, address , card_number, expiry_month, expiry_year, cvv FROM  jahatt";
+        $result = $conn->query($sql);
+
+        if (!$result) {
+            die("Query failed :" . $conn->error);
+        }
+          $serialNumber = 1;
+          
+          if($result-> num_rows > 0) {
+            while($row = $result ->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>".$serialNumber."</td>";
+                echo "<td>".$row["name"]."</td>";
+                echo "<td>".$row["phone"]."</td>";
+                echo "<td>".$row["address"]."</td>";
+                echo "<td>".$row["card_number"]."</td>";
+                echo "<td>".$row["expiry_month"]."</td>";
+                echo "<td>".$row["expiry_year"]."</td>";
+                echo "<td>".$row["cvv"]."</td>";
+                echo "</tr>";
+                $serialNumber++;
+            }
+          }else{
+            echo "<tr><td colspan='8'>No payments found </td></tr>";
+          }
+          $conn->close();
+    ?>
+     </table>
+</body>
+</html>
